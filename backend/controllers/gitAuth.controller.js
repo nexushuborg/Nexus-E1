270ee3ca9b717dc7b76da7b9a3c63cfe66087dcf
Arin <arinkumarjoshi@gmail.com>
2025-08-12 12:51:04 +0530

    fixed routing for Oauth

--- backend/controllers/gitAuth.controller.js
+++ backend/controllers/gitAuth.controller.js
@@ -3,18 +3,18 @@ export const home = (req, res) => {
 };
 
 export const githubCallback = (req, res) => {
-    res.redirect("/api/auth/profile");
+    res.redirect("/api/github/profile");
 };
 
 export const profile = (req, res) => {
     if (!req.isAuthenticated()) return res.redirect("/");
-    res.send(`<h2>Hello, ${req.user.username}</h2><a href="/api/auth/logout">Logout</a>`);
+    res.send(`<h2>Hello, ${req.user.username}</h2><a href="/api/github/logout">Logout</a>`);
 };
 
 export const logout = (req, res) => {
     req.logout(() => {
         res.clearCookie("connect.sid");
         res.clearCookie("jwt");
-        res.redirect("/api/auth/home");
+        res.redirect("/api/github/home");
     });
 };
