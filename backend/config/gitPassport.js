0c3a3e0bf89e9dc0eb68ac286fa702bc3cc26bf3
Waiz <164471173+decodewidwaiz@users.noreply.github.com>
2025-08-13 21:32:40 +0530

    Update gitPassport.js

--- backend/config/gitPassport.js
+++ backend/config/gitPassport.js
@@ -11,7 +11,7 @@ passport.use(
     {
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/api/auth/github/callback",
+      callbackURL: "http://localhost:5000/api/auth/github/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       const githubProfile = {
