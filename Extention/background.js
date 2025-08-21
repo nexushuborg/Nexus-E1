573d8460f9f1a2cf277e9803155343f74915882d
Arin <arinkumarjoshi@gmail.com>
2025-08-22 02:40:04 +0530

    implemented github oauth for the extension

--- Extention/background.js
+++ Extention/background.js
@@ -1,3 +1,120 @@
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("Extension installed");
+  console.log(chrome.storage.local);
+});
+
+// login
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "authWithGitHub") {
+
+    chrome.storage.local.get(["username", "jwt"], (stored) => {
+      if (stored.username && stored.jwt) {
+        console.log(`Already logged in as ${stored.username}`);
+        sendResponse({
+          message: "You are already logged in",
+          username: stored.username,
+          avatarUrl: stored.avatarUrl
+        });
+        return; // stop here
+      }
+    })
+
+    const redirectURL = chrome.identity.getRedirectURL();
+    const state = crypto.getRandomValues(new Uint32Array(1))[0].toString(16);
+    //save `state`
+    chrome.storage.session.set({ oauthState: state });
+    console.log("redirect url is " + redirectURL);
+
+    chrome.identity.launchWebAuthFlow({
+      url: `https://github.com/login/oauth/authorize?client_id=Ov23limgwxLHCgCu5kmJ&redirect_uri=${encodeURIComponent(redirectURL)}&state=${state}&scope=${encodeURIComponent("read:user repo")}`,
+      interactive: true
+    }, async (responseUrl) => {
+      if (chrome.runtime.lastError) {
+        console.error("Auth failed:", chrome.runtime.lastError);
+        sendResponse({ error: "Auth failed" });
+        return;
+      }
+      console.log("Auth response:", responseUrl);
+
+      // to get the code out of the responseUrl
+      const params = new URL(responseUrl).searchParams;
+      const code = params.get("code");
+      const returnedState = params.get("state");
+
+      // compare with stored state
+      //chrome.storage.session.get : returns a prom that is async
+      const { oauthState } = await chrome.storage.session.get("oauthState");
+      if (returnedState !== oauthState) {
+        console.log("Invalid state, possible CSRF attack.");
+        sendResponse({ error: "Invalid state" });
+        return;
+      }
+
+      // exchanges the code to get the token
+      const tokenResponse = await fetch("http://localhost:5000/api/auth/exchange-code", {
+        method: "POST",
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          code,
+          redirect_uri: redirectURL
+        })
+      });
+
+      const tokenData = await tokenResponse.json();
+      console.log("Access token response:", tokenData);
+
+      // only for authenticated users
+      const userRes = await fetch("https://api.github.com/user", {
+        headers: { Authorization: `Bearer ${tokenData.accessToken}` }
+      });
+      const userData = await userRes.json();
+      console.log("User profile response:", userData);
+
+      await chrome.storage.local.set({
+        jwt: tokenData.jwt,
+        username: userData.login,
+        avatarUrl: userData.avatar_url,
+        scopes: userData.scopes
+      });
+
+      sendResponse({
+        jwt: tokenData.jwt,
+        username: userData.login,
+        avatarUrl: userData.avatar_url,
+        scopes: userData.scopes
+      });
+    });
+
+    return true;
+  }
+});
+
+//logout:just clear the local storage
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "logout") {
+    // debug
+    chrome.storage.local.get(["jwt", "username", "avatarUrl", "scopes"], (result) => {
+      console.log("Stored data:", result);
+
+      if (result.jwt) {
+        console.log("Username:", result.username);
+        console.log("jwt:", result.jwt);
+        console.log("Avatar URL:", result.avatarUrl);
+        console.log("Scopes:", result.scopes);
+      } else {
+        console.log("No user logged in.");
+      }
+    });
+    chrome.storage.local.clear().then(() => {
+      console.log("All user data cleared.");
+      sendResponse({ success: true });
+    });
+    return true;
+  }
+});
 
 function checkEvent() {
   console.log('listening to event');
@@ -9,11 +126,11 @@ function checkEvent() {
     })
   })
 }
-  
-  const GFG_SUBMIT_URL = 'https://practiceapiorigin.geeksforgeeks.org/api/latest/problems/submission/submit/result/';
+
+const GFG_SUBMIT_URL = 'https://practiceapiorigin.geeksforgeeks.org/api/latest/problems/submission/submit/result/';
 const CODECHEF_SUBMIT_URL = 'https://www.codechef.com/api/ide/submit';
 
-  chrome.webRequest.onCompleted.addListener(
+chrome.webRequest.onCompleted.addListener(
   (details) => {
     let scriptToInject = null;
 
@@ -27,7 +144,7 @@ const CODECHEF_SUBMIT_URL = 'https://www.codechef.com/api/ide/submit';
     }
 
     chrome.scripting.executeScript({
-      target: {tabId: details.tabId},
+      target: { tabId: details.tabId },
       func: checkEvent
     });
 
@@ -36,9 +153,9 @@ const CODECHEF_SUBMIT_URL = 'https://www.codechef.com/api/ide/submit';
         chrome.scripting.executeScript({
           target: { tabId: details.tabId },
           files: [scriptToInject],
-          world: 'MAIN' 
+          world: 'MAIN'
         });
-      }, 1500); 
+      }, 1500);
     }
   },
   {
@@ -48,19 +165,18 @@ const CODECHEF_SUBMIT_URL = 'https://www.codechef.com/api/ide/submit';
     ]
   }
 );
-    
-    chrome.runtime.onMessage.addListener((request, sender, sendRes) => {
 
-      //debug
-    console.log('Received the message', request.id);
+chrome.runtime.onMessage.addListener((request, sender, sendRes) => {
 
-      if (request.id === 'GfgSoln') {
-        console.log("GFG response");
-        console.log(request);
-      }
-      else if (request.id === 'CfSoln')
-        console.log("Codechef response");
-        console.log(request);
-        
-    });
-    
+  //debug
+  console.log('Received the message', request.id);
+
+  if (request.id === 'GfgSoln') {
+    console.log("GFG response");
+    console.log(request);
+  }
+  else if (request.id === 'CfSoln')
+    console.log("Codechef response");
+  console.log(request);
+
+});
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,6 +2,7 @@ import express from "express"
 import passport from "passport";
 import { githubCallback, logout, profile } from "../controllers/auth.controller.js";
 import { protect } from "../middlewares/auth.middlewares.js";
+import jwt from "jsonwebtoken"
 
 const router = express.Router();
 
@@ -22,5 +23,67 @@ router.get("/profile", protect, profile);
 
 // logout button hit this endpoint
 router.get("/logout", logout);
+
+// endpoint to exchange the code ecnoded in the url in return of the github access token
+router.post("/exchange-code", async (req, res) => {
+    try {
+        const { code, redirect_uri } = req.body;
+
+        const params = new URLSearchParams({
+            client_id: process.env.GITHUB_CLIENT_ID,
+            client_secret: process.env.GITHUB_CLIENT_SECRET,
+            code,
+            redirect_uri
+        });
+
+        const response = await fetch("https://github.com/login/oauth/access_token", {
+            method: "POST",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            body: params
+        });
+
+        const tokenData = await response.json();
+        console.log(tokenData.access_token);
+        if (!tokenData.access_token) {
+            return res.status(400).json({ error: "Failed to get access token", details: tokenData });
+        }
+
+        // fetch GitHub user profile
+        const userResponse = await fetch("https://api.github.com/user", {
+            headers: { Authorization: `Bearer ${tokenData.access_token}` }
+        });
+        const ghUser = await userResponse.json();
+
+        // sign JWT
+        const jwtToken = jwt.sign(
+            {
+                sub: ghUser.id,
+                username: ghUser.login,
+                avatarUrl: ghUser.avatar_url,
+                scopes: tokenData.scope,
+                accessToken: tokenData.access_token
+            },
+            process.env.JWT_SECRET,
+            { expiresIn: "1h" }
+        );
+
+        //sends back everything to background.js
+        res.json({
+            repos: ghUser.repos,
+            accessToken: tokenData.access_token,
+            jwt: jwtToken,
+            username: ghUser.login,
+            avatarUrl: ghUser.avatar_url,
+            scopes: tokenData.scope
+        });
+
+    } catch (error) {
+        console.error("Error exchanging code:", error);
+        res.status(500).json({ error: error.message });
+    }
+});
 
 export default router;
